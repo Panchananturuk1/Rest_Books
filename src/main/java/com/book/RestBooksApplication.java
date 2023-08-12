9f5610f1083e9f38e7dd26c6e7e3764d69f2c93e
panchanan-turuk <pturuk123@gmail.com>
2023-08-12 17:54:17 +0530

    file added

---
+++ src/main/java/com/book/RestBooksApplication.java
@@ -0,0 +1,13 @@
+package com.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestBooksApplication.class, args);
+	}
+
+}
